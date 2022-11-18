6254ec1ef7dc14a859f496e4c3944ddbe0b2c7ac
Lion <72764899+LionTara@users.noreply.github.com>
2022-11-18 14:11:36 +0100

    Add files via upload

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,7 +7,7 @@ import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles.js';
 
 
-const Map = ({ setCoordinates, setBounds, coordinates}) => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width: 600px)');
 
@@ -23,9 +23,10 @@ const Map = ({ setCoordinates, setBounds, coordinates}) => {
                 onChange={(e) => {
                     console.log(e);
                     setCoordinates({ lat: e.center.lat, lng: e.center.lng });
-                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                    setBounds({ sw: e.marginBounds.sw, ne: e.marginBounds.ne });
                 }}
                 onChildClick = {''}
+                
             >
             </GoogleMapReact>
         </div>
